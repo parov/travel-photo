9730ea4601402cee69e6744cbba26228ac629194
Parov <pierangelo.turchiarelli@gmail.com>
2013-05-08 17:17:35 +0200

    Adding before block to test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe "Posts" do
+  before { visit new_post_path }
+
   it "should have the content 'wandereyes'" do
-    visit new_post_path
     page.should have_content('wandereyes')
   end
 end
